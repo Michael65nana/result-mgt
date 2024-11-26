c8aaec32dedd2d49d9b5c5d76cebc71a3f3ca06d
michael <105092313+law65horizon@users.noreply.github.com>
2024-11-27 00:57:39 +0100

    Update FireBase.js

--- src/FireBase/FireBase.js
+++ src/FireBase/FireBase.js
@@ -5,15 +5,6 @@ import { getFirestore } from "firebase/firestore";
 import {getStorage} from 'firebase/storage'
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCYdyw0RtoONUHKJ4zxaYScrFHUceBJFgE",
-    authDomain: "insider-4efaa.firebaseapp.com",
-    projectId: "insider-4efaa",
-    storageBucket: "insider-4efaa.appspot.com",
-    messagingSenderId: "228918648775",
-    appId: "1:228918648775:web:772efab4b57eeee8bb43e2",
-    measurementId: "G-3D648EBL2D"
-  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
